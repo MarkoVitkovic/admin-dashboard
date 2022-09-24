b5c2f63b190846f08f8f1ecf420684f846bd070b
Marko Vitkovic <marko.vitkovic@outlook.com>
2022-09-24 16:19:08 +0200

    create revenue section, add charts

--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, StackingColumnSeries, Tooltip } from '@syncfusion/ej2-react-charts/src'
+import { stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis } from '../../data/dummy'
 
-const Stacked = () => {
+const Stacked = ({width, heigth}) => {
   return (
-    <div>
-      
-    </div>
+    <ChartComponent width={width} height={heigth} id="charts" primaryXAxis={stackedPrimaryXAxis} primaryYAxis={stackedPrimaryYAxis} chartArea={{border: {width: 0}}} tooltip={{enable:true}} legendSettings={{background: "white"}}>
+      <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]} />
+      <SeriesCollectionDirective>
+        {stackedCustomSeries.map((item, index) => <SeriesDirective key={index} {...item} />)}
+      </SeriesCollectionDirective>
+    </ChartComponent>
   )
 }
 
