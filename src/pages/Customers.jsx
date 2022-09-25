70804026efe298ba9166df56de59348fcb5c349e
Marko Vitkovic <marko.vitkovic@outlook.com>
2022-09-25 23:00:27 +0200

    add modal for deleting confirmation

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -7,7 +7,7 @@ const Customers = () => {
   return (
     <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
       <Header category="Page" title="Customers" />
-      <GridComponent dataSource={customersData} allowPaging allowSorting toolbar={['Delete']} editSettings={{ allowDeleting: true, allowEditing: true}} width="auto">
+      <GridComponent dataSource={customersData} allowPaging allowSorting toolbar={['Delete']} editSettings={{ allowDeleting: true, allowEditing: true, showDeleteConfirmDialog: true}} width="auto">
         <ColumnsDirective>
           {customersGrid.map((item, index) => (
             <ColumnDirective key={index} {...item} />
